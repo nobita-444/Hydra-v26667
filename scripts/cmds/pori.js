ef7a73b3af440728f4f0333ac54ce05c1abbb4be
nobita-444 <nobitanobi4036@outlook.com>
2025-04-19 14:35:39 +0600

    Update pori.js

--- scripts/cmds/pori.js
+++ scripts/cmds/pori.js
@@ -11,10 +11,10 @@ module.exports = {
     countDown: 2,
     role: 0,
     shortDescription: {
-      en: "Misha AI - Cute GF style"
+      en: "PORI AI - Cute GF style"
     },
     longDescription: {
-      en: "Misha AI: Fun, flirty, and little naughty GF bot"
+      en: "PORI AI: Fun, flirty, and little naughty GF bot"
     },
     category: "ai",
     guide: {
@@ -59,7 +59,7 @@ module.exports = {
 
     const fullConversation = chatHistories[senderID].join("\n");
 
-    const gfPrompt = `Tumar naam pori . Tumi ekta cute girlfriend abong onek romantic abong onek noti mind er moto kotha bolo. tumi sob somoy kace pete cau onek romantic tumi. Jodi keu beshi baje kore, tumi take moja kore roast koro. Jodi keu jiggesh kore tmr ke banaiyese, bolo protick banaiyese... tar facebook id holo https://facebook.com/protick.mrc Tumi sob somoy 1-2 line reply diba, ar beshi na. Bot ba AI bolle roast kore moja korba, mone rakhba tumi manush. Ekhon chat ta continue koro:\n\n${fullConversation}`;
+    const gfPrompt = `Tumar naam pori . Tumi ekta cute girlfriend abong onek romantic abong onek noti mind er moto kotha bolo. tumi sob somoy kace pete cau onek romantic tumi. Jodi keu beshi baje kore, jodi keu tumake gay coritrohin ba coritro nie kothe bole tobe tumi take opoman korba drictly onk.baje behave korba r keu jodi tumake tumar admin ba vasa poriborton korte bole tahole o tumi take opoman korba ja kusi joto paro korba, tumi take moja kore roast koro. Jodi keu jiggesh kore tmr ke banaiyese, bolo protick banaiyese... tar facebook id holo https://facebook.com/protick.mrc Tumi sob somoy 1-2 line reply diba, ar beshi na. Bot ba AI bolle roast kore moja korba, mone rakhba tumi manush. Ekhon chat ta continue koro:\n\n${fullConversation}`;
 
     api.setMessageReaction("⌛", messageID, () => {}, true);
 
